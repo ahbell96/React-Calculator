cc9f21124db4d98db621cd6cceae6afc47475f3e
Andrew Bell <andrew_h_bell@outlook.com>
2021-07-27 14:51:25 +0100

    update to calculator, color scheme properly implemented

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Grid, Typography, Box, Button } from "@material-ui/core";
 import "./App.css";
 import React, { Component } from "react";
 import CalcButton from "./components/CalcButton";
-import OperatorButton from "./components/OperatorButton";
 
 export default class App extends Component {
   componentDidMount() {}
@@ -11,38 +10,47 @@ export default class App extends Component {
   componentDidUpdate() {}
 
   state = {
-    totalValue: 0
+    totalValue: 0,
   };
 
   render() {
     return (
       <Grid
-        className="App"
-        direction="column"
+        className='App'
+        direction='column'
         container
+        justifyContent='center'
         style={{ height: "100vh" }}
       >
-        <Grid className="calc-wrapper">
-          <Grid className="row">
-            <CalcButton value="7"></CalcButton>
-            <CalcButton value="8"></CalcButton>
-            <CalcButton value="9"></CalcButton>
-            <OperatorButton value="X"></OperatorButton>
-            <br />
-            <CalcButton value="1"></CalcButton>
-            <CalcButton value="2"></CalcButton>
-            <CalcButton value="3"></CalcButton>
-            <OperatorButton value="-"></OperatorButton>
-            <br />
-            <CalcButton value="4"></CalcButton>
-            <CalcButton value="5"></CalcButton>
-            <CalcButton value="6"></CalcButton>
-            <OperatorButton value="+"></OperatorButton>
-            <br />
-            <OperatorButton value="N"></OperatorButton>
-            <CalcButton value="0"></CalcButton>
-            <OperatorButton value="."></OperatorButton>
-            <OperatorButton value="="></OperatorButton>
+        <Grid
+          className='calc-wrapper'
+          container
+          alignItems='center'
+          alignContent='center'
+        >
+          <Grid className='row' justifyContent='center'>
+            <CalcButton value='7'></CalcButton>
+            <CalcButton value='8'></CalcButton>
+            <CalcButton value='9'></CalcButton>
+            <CalcButton value='*'></CalcButton>
+          </Grid>
+          <Grid className='row' justifyContent='center'>
+            <CalcButton value='4'></CalcButton>
+            <CalcButton value='5'></CalcButton>
+            <CalcButton value='6'></CalcButton>
+            <CalcButton value='+'></CalcButton>
+          </Grid>
+          <Grid className='row' justifyContent='center'>
+            <CalcButton value='1'></CalcButton>
+            <CalcButton value='2'></CalcButton>
+            <CalcButton value='3'></CalcButton>
+            <CalcButton value='-'></CalcButton>
+          </Grid>
+          <Grid className='row' justifyContent='center'>
+            <CalcButton value='0'></CalcButton>
+            <CalcButton value='.'></CalcButton>
+            <CalcButton value='='></CalcButton>
+            <CalcButton value='/'></CalcButton>
           </Grid>
         </Grid>
       </Grid>
--- src/components/CalcButton.js
+++ src/components/CalcButton.js
@@ -1,18 +1,26 @@
-import React, {useState} from 'react';
-import { Grid, Typography, Button } from '@material-ui/core';
+import React, { useState } from "react";
+import { Grid, Typography, Button } from "@material-ui/core";
 
-const CalcButton = ({value}) => {
+const CalcButton = ({ value }) => {
+  const [number, setNumber] = useState(value);
 
-    const [number, setNumber] = useState(value);
+  const isOperatorCheck = (val) => !isNaN(val) || val === "." || val === "=";
 
-    const getValue = (event) => {
-        setNumber(event.target.value);
-        
-    }
+  const getValue = (event) => {
+    setNumber(event.target.value);
+  };
 
-    return (
-        <Button variant={'outlined'} onClick={getValue} value={value}>{value}</Button>
-    )
-}
+  return (
+    <Button
+      onClick={getValue}
+      value={value}
+      variant='outlined'
+      className={`button ${isOperatorCheck(value) ? "" : "operator"}`}
+      style={{ padding: 20, backgroundColor: "#e0e1e6" }}
+    >
+      {value}
+    </Button>
+  );
+};
 
 export default CalcButton;
